0ac3fc95f623cc20edae508be0b874c58f5af857
AntonT <turchenkoav96@gmail.com>
2018-04-03 09:36:42 +0300

    Initial commit

---
+++ app/src/main/java/com/antont/issuestracker/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.antont.issuestracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
